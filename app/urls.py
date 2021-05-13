6b7be76b0432dd6ae51f99db5ad290449f815634
avishmehta68710 <avishmehta68710@gmail.com>
2021-05-14 00:45:37 +0400

    Task

--- app/urls.py
+++ app/urls.py
@@ -29,5 +29,8 @@ urlpatterns = [
     path('api/token',TokenObtainPairView.as_view()),
     path('api/token/refresh/',TokenRefreshView.as_view()),
     
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
