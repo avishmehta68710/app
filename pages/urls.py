792cf943507168bf3df343a3ee848de5a62e15af
avishmehta68710 <avishmehta68710@gmail.com>
2021-05-14 10:16:25 +0400

    1

--- pages/urls.py
+++ pages/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # path('api/users/',views.ListUsers.as_view()),
     # path('api/token/auth/', views.CustomAuthToken.as_view()),
     path('',views.index,name="index"),
-    path('admin/advisor/',views.admin_page,name="Admin Page"),
+    path('admins/advisor/',views.admin_page,name="Admin Page"),
     path('user/register/',views.register,name="register"),
     path('user/login/',views.login_user,name="login"),
     path('user/<int:id>/advisor/',views.advisor_list,name="COntent List"),
