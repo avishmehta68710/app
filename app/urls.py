3e74c14b8cba170b05fad04760c0122e7dcf5728
avishmehta68710 <avishmehta68710@gmail.com>
2021-05-14 10:01:24 +0400

    1

--- app/urls.py
+++ app/urls.py
@@ -19,8 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 # from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
-from django.views.static import serve
-from django.conf.urls import url
+# from django.views.static import serve
+# from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,7 +30,7 @@ urlpatterns = [
     # path('api/token/refresh/',TokenRefreshView.as_view()),
     
     
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
